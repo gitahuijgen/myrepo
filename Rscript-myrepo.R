92b12643ac2070d38bf33010d73ac1d366c5f97c
Gita Huijgen <80745ghu@eur.nl>
2022-08-05 13:57:26 +0200

    commit from Rstudio

---
+++ Rscript-myrepo.R
@@ -0,0 +1,10 @@
+#see if this appears in github
+
+
+rm(list = ls())
+
+dta <- read.csv("C:/users/80745ghu/OneDrive - Erasmus University Rotterdam/Documents/PhD 1/Achtergrondvars gemeenten/Regionale_kerncijfers_vergrijzing-dichtheid-inkomen-totaal2017 - MERGER.csv",
+                sep = ";")
+
+
+head(dta)
